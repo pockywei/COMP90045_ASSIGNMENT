5c4d821a0927f7326a4823946d1e7649c698e19f
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2016-04-11 03:41:41 +1000

    pretty printer is pretty much finished, need to find a way to pretty print the right format of arith expr

---
+++ sprout_bean/bean_pprint.ml
@@ -0,0 +1,8 @@
+open Bean_ast
+open Format
+
+let print_program fmt prog = (Printf.printf " \nwhat is in side => \n" ;
+	Format.set_max_indent 100;
+	printTypedefList fmt prog.typedefs;
+	printFuncdefList fmt prog.funcdefs
+)
